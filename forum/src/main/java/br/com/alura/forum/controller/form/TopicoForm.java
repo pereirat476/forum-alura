6eb6a55c0f1358ec753cbfdf088fd1081f3426cb
Tiago Santos <pereirat476@gmail.com>
2020-10-13 16:56:36 -0300

    commit inicial

---
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.controller.form;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor @AllArgsConstructor
+public class TopicoForm {
+
+	private String titulo;
+	private String mensagem;
+	private String nomeCurso;
+
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
+}
